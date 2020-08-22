d22c39edac9f1270af9b9ebd114a86938c044254
john.ku <dongwoo.john.ku@gmail.com>
2020-08-22 19:30:47 +0900

    hardcoding to loading
    refer to conf file

--- hello-opcua-logstash.py
+++ hello-opcua-logstash.py
@@ -1,45 +1,52 @@
 import sys
 sys.path.insert(0, "..")
-from opcua import Client
+import time
 import logging
+import configparser
 import logstash
+from opcua import Client
 
-host = 'localhost'
+config = configparser.ConfigParser()
+config.read('./communication.conf')
+end_point = config.get('MAIN', 'opcua_server_endpoint')
+logging_interval = config.get('MAIN', 'logging_interval')
+logstash_host = config.get('MAIN', 'logstash_host')
+logstash_port = int(config.get('MAIN', 'logstash_port'))
+
+opc_nodeid = config.get('MAIN', 'opc_nodeid')
+opc_timeout = int(config.get('MAIN', 'opc_timeout'))
+
+# config = config['MAIN']
+# print(config['opcua_server_endpoint'])
+
 test_logger = logging.getLogger('ua-logger')
 test_logger.setLevel(logging.INFO)
-test_logger.addHandler(logstash.LogstashHandler(host, 5959, version=1))
+test_logger.addHandler(logstash.LogstashHandler(logstash_host, logstash_port, version=1))
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.WARN)
     #logger = logging.getLogger("KeepAlive")
     #logger.setLevel(logging.DEBUG)
+    client = Client(url=end_point)
 
-    client = Client("opc.tcp://host.docker.internal:53530/OPCUA/SimulationServer")
-    # client = Client("opc.tcp://SEOUL-DKU.apac.turck.info:53530/OPCUA/SimulationServer")
-    # client = Client("opc.tcp://SEOUL-DKU.mshome.net:53530/OPCUA/SimulationServer")
+    while True:
+        try:
+            client.connect()
+        except:
+            print('connection failed')
+        else:
+            # client.load_type_definitions()  # load definition of server specific structures/extension objects
+            varList = client.get_node(opc_nodeid).get_children()
 
-    try:
-        client.connect()
-        client.load_type_definitions()  # load definition of server specific structures/extension objects
+            for var in varList :
+                tagName ='"tagName":' + '"' + str(var.get_browse_name()).split(":")[1][:-1] + '"'
+                tagValue ='"tagValue":' + '"' + str(var.get_value()) + '"'
+                # timestamp = (str(var.get_data_value()).split(",")[-1][1:-1]).split(':')
+                # timestamp ='"' + timestamp[0] + '":"' + timestamp[1] +'"'
+                logData = ([tagName, tagValue])
+                logData = '{' + ", ".join(logData) +'}'
+                test_logger.info(logData)
 
-        # Client has a few methods to get proxy to UA nodes that should always be in address space such as Root or Objects
-        root = client.get_root_node()
-        objects = client.get_objects_node()
-        # Specific Node which I want to access
-        # print("Object's children node are: ", objects.get_children()[2])
-        # print(objects.get_children()[2])
-        # print('')
+            client.disconnect()
+            time.sleep(int(logging_interval) / 1000)
 
-        varList = objects.get_children()[2].get_children()
-
-        for var in varList :
-            tagName ='"tagName":' + '"' + str(var.get_browse_name()).split(":")[1][:-1] + '"'
-            tagValue ='"tagValue":' + '"' + str(var.get_value()) + '"'
-            # timestamp = (str(var.get_data_value()).split(",")[-1][1:-1]).split(':')
-            # timestamp ='"' + timestamp[0] + '":"' + timestamp[1] +'"'
-            logData = ([tagName, tagValue])
-            logData = '{' + ", ".join(logData) +'}'
-            test_logger.info(logData)
-
-    finally:
-        client.disconnect()
